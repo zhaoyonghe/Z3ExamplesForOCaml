471ab95249547024a6bd9388db7978f471254de5
zhaoyonghe <909672941@qq.com>
2020-05-07 08:33:39 -0400

    14,15,16

--- aa.ml
+++ aa.ml
@@ -16,6 +16,7 @@ open Z3.Arithmetic.Integer
 open Z3.Arithmetic.Real
 open Z3.BitVector
 open Z3.Quantifier
+open Z3.FloatingPoint
 
 let print_check (solver: Solver.solver): unit =
 	Printf.printf "%s\n" (string_of_status (Solver.check solver []))
@@ -35,8 +36,21 @@ let print_proof (solver: Solver.solver): unit =
 		| Some p -> Printf.printf "%s\n" (Expr.to_string p)
 		| None -> Printf.printf "no proof\n"		
 	with
-	| _ -> Printf.printf "no get_proof\n"
+	| _ -> Printf.printf "no proof\n"
 	(* It should return None, but actually raises error. *)
+
+let solve (ctx: context) (assertions: Expr.expr list): unit =
+	let solver = (mk_solver ctx None) in
+	Solver.add solver assertions;
+	print_check solver;
+	print_model solver
+
+let prove (ctx: context) (assertions: Expr.expr list): unit =
+	let solver = (mk_solver ctx None) in
+	Solver.add solver assertions;
+	print_check solver;
+	print_model solver;
+	print_proof solver
 
 (*
 	from z3 import *
@@ -54,15 +68,12 @@ let test1 (ctx: context): unit =
 	let bool_sort: Sort.sort = Boolean.mk_sort ctx in
 	let tie_expr: Expr.expr = Expr.mk_const ctx tie bool_sort in 
 	let shirt_expr: Expr.expr = Expr.mk_const ctx shirt bool_sort in 
-	let solver = (mk_solver ctx None) in
-	(Solver.add solver [
-		 Boolean.mk_or ctx [tie_expr; shirt_expr]; 
-		 (* Boolean.mk_or ctx [(Boolean.mk_not ctx shirt_expr); tie_expr]; unsatisfiable *)
-		 Boolean.mk_or ctx [(Boolean.mk_not ctx tie_expr); shirt_expr];
-		 Boolean.mk_or ctx [(Boolean.mk_not ctx tie_expr); (Boolean.mk_not ctx shirt_expr)]
-		];
-	print_check solver;
-	print_model solver)
+	solve ctx [
+		Boolean.mk_or ctx [tie_expr; shirt_expr]; 
+		(* Boolean.mk_or ctx [(Boolean.mk_not ctx shirt_expr); tie_expr]; unsatisfiable *)
+		Boolean.mk_or ctx [(Boolean.mk_not ctx tie_expr); shirt_expr];
+		Boolean.mk_or ctx [(Boolean.mk_not ctx tie_expr); (Boolean.mk_not ctx shirt_expr)]
+	]
 
 (*
 	Z = IntSort()
@@ -91,10 +102,8 @@ let test2 (ctx: context): unit =
 	let temp2: Expr.expr = FuncDecl.apply f [Arithmetic.mk_add ctx [Arithmetic.mk_sub ctx [y; x]; one]] in
 	let implies_r: Expr.expr = Boolean.mk_eq ctx temp1 temp2 in
 	let fml: Expr.expr = Boolean.mk_implies ctx implies_l implies_r in
-	let solver = (mk_solver ctx None) in
-	Solver.add solver [Boolean.mk_not ctx fml];
-	print_check solver;
-	print_model solver
+	solve ctx [Boolean.mk_not ctx fml]
+
 
 (*
 	from z3 import *
@@ -127,11 +136,11 @@ let test3 (ctx: context): unit =
 let test4 (ctx: context): unit =
 	let s_sort: Sort.sort = Sort.mk_uninterpreted_s ctx "S" in
 	let s: Expr.expr = Expr.mk_const_s ctx "s" s_sort in
-	let q: Quantifier.quantifier = Quantifier.mk_forall_const ctx [s] (Boolean.mk_not ctx (Boolean.mk_eq ctx s s)) (Some 1) [] [] None None in
-	let solver = (mk_solver ctx None) in
-	Solver.add solver [Quantifier.expr_of_quantifier q];
-	print_check solver;
-	print_model solver
+	let q: Quantifier.quantifier = 
+		Quantifier.mk_forall_const ctx [s] (Boolean.mk_not ctx (Boolean.mk_eq ctx s s)) 
+		(Some 1) [] [] None None in
+	solve ctx [Quantifier.expr_of_quantifier q]
+
 
 (*
 	Z = IntSort()
@@ -149,10 +158,7 @@ let test5 (ctx: context): unit =
 	let a: Expr.expr = Boolean.mk_const_s ctx "a" in
 	let one: Expr.expr = Arithmetic.Integer.mk_numeral_i ctx 1 in
 	let fml: Expr.expr = FuncDecl.apply g [Arithmetic.mk_add ctx [one; (FuncDecl.apply f [a])]] in
-	let solver = (mk_solver ctx None) in
-	Solver.add solver [fml];
-	print_check solver;
-	print_model solver
+	solve ctx [fml]
 
 (*
 	x = Int('x')
@@ -178,7 +184,6 @@ let test6 (ctx: context): unit =
 	Printf.printf "operator: %s\n" (FuncDecl.to_string (Expr.get_func_decl n));
 	Printf.printf "op name: %s\n" (Symbol.to_string (FuncDecl.get_name (Expr.get_func_decl n)))
 
-
 (*
 	solve([y == x + 1, ForAll([y], Implies(y <= 0, x < y))])
 	<==> alpha conversion
@@ -194,10 +199,10 @@ let test7 (ctx: context): unit =
 		Quantifier.mk_forall_const ctx [y] 
 		(Boolean.mk_implies ctx (Arithmetic.mk_le ctx y zero) (Arithmetic.mk_lt ctx x y)) 
 		(Some 1) [] [] None None in
-	let solver = (mk_solver ctx None) in
-	Solver.add solver [Boolean.mk_eq ctx y (Arithmetic.mk_add ctx [x; one]); Quantifier.expr_of_quantifier q];
-	print_check solver;
-	print_model solver
+	solve ctx [
+		Boolean.mk_eq ctx y (Arithmetic.mk_add ctx [x; one]); 
+		Quantifier.expr_of_quantifier q
+	]
 
 (*
 	solve([y == x + 1, ForAll([y], Implies(y <= 0, x > y))])
@@ -214,10 +219,11 @@ let test8 (ctx: context): unit =
 		Quantifier.mk_forall_const ctx [y] 
 		(Boolean.mk_implies ctx (Arithmetic.mk_le ctx y zero) (Arithmetic.mk_gt ctx x y)) 
 		(Some 1) [] [] None None in
-	let solver = (mk_solver ctx None) in
-	Solver.add solver [Boolean.mk_eq ctx y (Arithmetic.mk_add ctx [x; one]); Quantifier.expr_of_quantifier q];
-	print_check solver;
-	print_model solver
+	solve ctx [
+		Boolean.mk_eq ctx y (Arithmetic.mk_add ctx [x; one]); 
+		Quantifier.expr_of_quantifier q
+	]
+
 
 (*
 	m, m1 = Array('m', Z, Z), Array('m1', Z, Z)
@@ -278,15 +284,16 @@ let test11 (ctx: context): unit =
 	let f_func: FuncDecl.func_decl = FuncDecl.mk_func_decl_s ctx "f" [s_sort] s_sort in
 	let x: Expr.expr = Expr.mk_const_s ctx "x" s_sort in
 	let f (x: Expr.expr): Expr.expr = (FuncDecl.apply f_func [x]) in
-	let solver = (mk_solver ctx None) in
-	Solver.add solver [Boolean.mk_eq ctx (f (f x)) x; Boolean.mk_eq ctx (f (f (f x))) x];
-	print_check solver;
-	print_model solver;
-	Solver.reset solver;	
-	Solver.add solver [Boolean.mk_eq ctx (f (f x)) x; 
-		Boolean.mk_eq ctx (f (f (f x))) x; Boolean.mk_not ctx (Boolean.mk_eq ctx (f x) x)];
-	print_check solver;
-	print_model solver
+	solve ctx [
+		Boolean.mk_eq ctx (f (f x)) x; 
+		Boolean.mk_eq ctx (f (f (f x))) x
+	];
+	solve ctx [
+		Boolean.mk_eq ctx (f (f x)) x; 
+		Boolean.mk_eq ctx (f (f (f x))) x; 
+		Boolean.mk_not ctx (Boolean.mk_eq ctx (f x) x)
+	]
+
 
 let mk_neq (ctx: context) (a: Expr.expr) (b: Expr.expr): Expr.expr = 
 	Boolean.mk_not ctx (Boolean.mk_eq ctx a b)
@@ -314,12 +321,14 @@ let test12 (ctx: context): unit =
 	let g_func: FuncDecl.func_decl = FuncDecl.mk_func_decl_s ctx "g" [s_sort] s_sort in
 	let f (x: Expr.expr) (y: Expr.expr): Expr.expr = (FuncDecl.apply f_func [x; y]) in
 	let g (x: Expr.expr): Expr.expr = (FuncDecl.apply g_func [x]) in
-	let solver = (mk_solver ctx None) in
-	Solver.add solver [Boolean.mk_eq ctx a b; Boolean.mk_eq ctx b c;
-			Boolean.mk_eq ctx d e; Boolean.mk_eq ctx b s; Boolean.mk_eq ctx d t;
-			mk_neq ctx (f a (g d)) (f (g e) b)];
-	print_check solver;
-	print_model solver
+	solve ctx [
+		Boolean.mk_eq ctx a b; Boolean.mk_eq ctx b c;
+		Boolean.mk_eq ctx d e; 
+		Boolean.mk_eq ctx b s; 
+		Boolean.mk_eq ctx d t;
+		mk_neq ctx (f a (g d)) (f (g e) b)
+	]
+
 
 (*
 	x, y = Reals('x y')
@@ -334,17 +343,124 @@ let test13 (ctx: context): unit =
 	let y: Expr.expr = Arithmetic.Real.mk_const_s ctx "y" in
 	let temp1: Expr.expr = Arithmetic.mk_add ctx [x; y] in
 	let temp2: Expr.expr = Arithmetic.mk_add ctx [x; Arithmetic.mk_mul ctx [two; y]] in
+	solve ctx [
+		Arithmetic.mk_ge ctx x zero; 
+		Boolean.mk_or ctx [(Arithmetic.mk_le ctx temp1 two); (Arithmetic.mk_ge ctx temp2 six)];
+		Boolean.mk_or ctx [(Arithmetic.mk_ge ctx temp1 two); (Arithmetic.mk_gt ctx temp2 four)]
+	]
+
+
+(*
+	A = Array('A', IntSort(), IntSort())
+	solve(A[x] == x, Store(A, x, y) == A)
+*)
+let test14 (ctx: context): unit =
+	let int_sort: Sort.sort = Arithmetic.Integer.mk_sort ctx in
+	let x: Expr.expr = Arithmetic.Integer.mk_const_s ctx "x" in
+	let y: Expr.expr = Arithmetic.Integer.mk_const_s ctx "y" in
+	let a_arr: Expr.expr = Z3Array.mk_const_s ctx "A" int_sort int_sort in
+	solve ctx [
+		Boolean.mk_eq ctx (Z3Array.mk_select ctx a_arr x) x;
+		Boolean.mk_eq ctx (Z3Array.mk_store ctx a_arr x y) a_arr
+	]
+	
+(*
+	check:
+
+	store:
+	s.add(Store(a, i, v)[j] == If(i == j, v, a[j]))
+	s.add(Store(a, i, v)[i] == v)
+
+	extensionality:
+	s.add(Implies(ForAll(i, a[i] == b[i]), a == b))
+	s.add(Implies(a[Ext(a, b)] == b[Ext(a, b)], a == b))
+*)
+let test15 (ctx: context): unit = 
+	let int_sort: Sort.sort = Arithmetic.Integer.mk_sort ctx in
+	let i: Expr.expr = Arithmetic.Integer.mk_const_s ctx "i" in
+	let j: Expr.expr = Arithmetic.Integer.mk_const_s ctx "j" in
+	let v: Expr.expr = Arithmetic.Integer.mk_const_s ctx "v" in
+	let a: Expr.expr = Z3Array.mk_const_s ctx "a" int_sort int_sort in
+	let b: Expr.expr = Z3Array.mk_const_s ctx "b" int_sort int_sort in
+	(* Store(a, i, v)[j] *)
+	let temp1: Expr.expr = Z3Array.mk_select ctx (Z3Array.mk_store ctx a i v) j in 
+	(* If(i == j, v, a[j]) *)
+	let temp2: Expr.expr = Boolean.mk_ite ctx (Boolean.mk_eq ctx i j) v (Z3Array.mk_select ctx a j) in
+	(* Store(a, i, v)[i] *)
+	let temp3: Expr.expr = Z3Array.mk_select ctx (Z3Array.mk_store ctx a i v) i in
+	(* ForAll(i, a[i] == b[i]) *)
+	let q: Quantifier.quantifier = 
+		Quantifier.mk_forall_const ctx [i] 
+		(Boolean.mk_eq ctx (Z3Array.mk_select ctx a i) (Z3Array.mk_select ctx b i)) 
+		(Some 1) [] [] None None in
+	(* a == b *)
+	let temp4: Expr.expr = Boolean.mk_eq ctx a b in
+	(* Ext(a, b) *)
+	let ext: Expr.expr = Z3Array.mk_array_ext ctx a b  in
+	(* a[Ext(a, b)] == b[Ext(a, b)] *)
+	let temp5: Expr.expr = Boolean.mk_eq ctx (Z3Array.mk_select ctx a ext) (Z3Array.mk_select ctx b ext) in
+	(* unsatisfiable, because these four axioms should all be true *)
+	solve ctx [
+		Boolean.mk_not ctx (Boolean.mk_and ctx [
+			Boolean.mk_eq ctx temp1 temp2;
+			Boolean.mk_eq ctx temp3 v;
+			Boolean.mk_implies ctx (Quantifier.expr_of_quantifier q) temp4;
+			Boolean.mk_implies ctx temp5 temp4
+		])
+	]	
+
+(*
+	def is_power_of_two(x):
+		return And(x != 0, 0 == (x & (x - 1)))
+	x = BitVec('x', 4)
+	prove(is_power_of_two(x) == Or([x == 2**i for i in range(4)]))
+*)
+let test16 (ctx: context): unit =
+	let zero: Expr.expr = Expr.mk_numeral_int ctx 0 (BitVector.mk_sort ctx 4) in
+	let one: Expr.expr = Expr.mk_numeral_int ctx 1 (BitVector.mk_sort ctx 4) in
+	let two: Expr.expr = Expr.mk_numeral_int ctx 2 (BitVector.mk_sort ctx 4) in
+	let four: Expr.expr = Expr.mk_numeral_int ctx 4 (BitVector.mk_sort ctx 4) in
+	let eight: Expr.expr = Expr.mk_numeral_int ctx 8 (BitVector.mk_sort ctx 4) in
+	let is_power_of_two (x: Expr.expr): Expr.expr = 
+		Boolean.mk_and ctx [
+			mk_neq ctx x zero;
+			Boolean.mk_eq ctx zero (BitVector.mk_and ctx x (BitVector.mk_sub ctx x one))
+		]
+	in
+	let x: Expr.expr = BitVector.mk_const_s ctx "x" 4 in 
 	let solver = (mk_solver ctx None) in
 	Solver.add solver [
-			Arithmetic.mk_ge ctx x zero; 
-			Boolean.mk_or ctx [(Arithmetic.mk_le ctx temp1 two); (Arithmetic.mk_ge ctx temp2 six)];
-			Boolean.mk_or ctx [(Arithmetic.mk_ge ctx temp1 two); (Arithmetic.mk_gt ctx temp2 four)]
-		];
+		Boolean.mk_not ctx (Boolean.mk_eq ctx (is_power_of_two x) (Boolean.mk_or ctx [
+																				Boolean.mk_eq ctx x one;
+																				Boolean.mk_eq ctx x two;
+																				Boolean.mk_eq ctx x four;
+																				Boolean.mk_eq ctx x eight
+																			]))
+	];
 	print_check solver;
-	print_model solver	
+	print_model solver;
+	print_proof solver
+
+(*
+	v = BitVec('v',32)
+	mask = v >> 31
+	prove(If(v > 0, v, -v) == (v + mask) ^ mask)
+*)
+let test17 (ctx: context): unit = ()
+
+(*
+	x = FP('x', FPSort(3, 4))
+	print(10 + x)
+*)
+let test18 (ctx: context): unit = ()(*
+	let fp_sort: Sort.sort = FloatingPoint.mk_sort ctx 3 4 in
+	let x: Expr.expr = FloatingPoint.mk_const_s ctx "x" fp_sort in
+	let ten: Expr.expr = FloatingPoint.mk_numeral_f ctx 10. fp_sort in
+	let temp: Expr.expr = FloatingPoint.mk_add ctx (FloatingPoint.RoundingMode.mk_rtz ctx) x ten in
+	Printf.printf "%s\n" (FloatingPoint.numeral_to_string temp)*)
 
 let _ = 
 	let cfg = [("model", "true"); ("proof", "true")] in
 	let ctx = (mk_context cfg) in
-	test13 ctx
+	test16 ctx
 ;;
